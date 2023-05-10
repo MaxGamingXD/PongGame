5de19790f73e78dd767a21719a7942d7f1dcc221
AIE\s220246 <vass63464@gmail.com>
2023-05-10 20:19:39 +0930

    Added: Ball bounce, sides resets ball position. (The Ball also is influenced by the paddle.)

--- PongGame/Assets/Scripts/BallMovement.cs
+++ PongGame/Assets/Scripts/BallMovement.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class BallMovement : MonoBehaviour
 {
-
     public Rigidbody2D rb_Ball;
+    public BallMovement bounce;
 
-    [SerializeField] private float right = 4f;
-    [SerializeField] private float left = -4f;
+    Vector2 original_Pos;
+
+
+    [SerializeField] private float right = 5f;
+    [SerializeField] private float left = -5f;
+    [SerializeField] private float timer = 2f;
+    private float original_Timer;
+    
+    //Floats for straight influenced hits//
+    /* private float straight_right = 7;
+    private float straight_left = -7; */
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody2D rb_Ball = GetComponent<Rigidbody2D>();
+        BallMovement bounce = GetComponent<BallMovement>();
+
+        original_Pos = rb_Ball.position;
+        original_Timer = timer;
+
     }
 
     // Update is called once per frame
@@ -29,8 +44,75 @@ public class BallMovement : MonoBehaviour
     {
         if(col.gameObject.CompareTag("Player"))
         {
+            PlayerMovement pm = col.gameObject.GetComponent<PlayerMovement>();
+            if(pm.left_Player == true)
+            {
+                if(pm.direction == 1) // UP Right
+                {
+                    rb_Ball.velocity = new Vector2(right, right);
+                }
+                else if (pm.direction == -1) // Down Right
+                {
+                    rb_Ball.velocity = new Vector2(right, left);
+                }
+                /*else // Right
+                {
+                    rb_Ball.AddForce(transform.forward * straight_right);
+                }*/
+                
+            }
+            else
+            {
+                if (pm.direction == 1) // UP Left
+                {
+                    rb_Ball.velocity = new Vector2(left, right);
+                }
+                else if (pm.direction == -1) // Down Left
+                {
+                    rb_Ball.velocity = new Vector2(left, left);
+                }
+                /*else // Left
+                {
+                    rb_Ball.AddForce(transform.forward * straight_left);
+                }*/
+            }
+           
             Debug.Log("ball hit ;)");
         }
-        
+        else if (col.gameObject.CompareTag("Point_Walls"))
+        {
+            // Stops ball movement when touched the left and right side walls, resets position, and sends the ball to the 
+            LeftandRightScore pm = col.gameObject.GetComponent<LeftandRightScore>();
+            if(pm.Is_Left == true)
+            {
+                gameObject.transform.position = original_Pos;
+                rb_Ball.velocity = Vector2.zero;
+                timer -= Time.deltaTime;
+                if (timer < 0)
+                {
+                    rb_Ball.isKinematic = false;
+                    rb_Ball.velocity = new Vector2(right, 0);
+                    timer = original_Timer;
+                    Debug.Log("Point Left");
+                }
+            }
+            else
+            {
+                gameObject.transform.position = original_Pos;
+                rb_Ball.velocity = Vector2.zero;
+                timer -= Time.deltaTime;
+                if (timer < 0)
+                {
+                    rb_Ball.isKinematic = false;
+                    rb_Ball.velocity = new Vector2(left, 0);
+                    timer = original_Timer;
+                    Debug.Log("Point Right");
+                }
+            }
+
+        }
+
     }
+    
+    
 }
--- PongGame/Assets/Scripts/PlayerMovement.cs
+++ PongGame/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public BoxCollider2D box;
+    public float direction = 0f; // 1 = up, -1 = down.
+    public bool left_Player = true;
 
-    [SerializeField] private float up = 4f;
-    [SerializeField] private float down = -4f;
+    [SerializeField] private float up = 5f;
+    [SerializeField] private float down = -5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,23 +23,55 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("w"))
+        if(left_Player == true)
         {
-            rb.velocity = new Vector2(0, up);
+            if (Input.GetKey("w"))
+            {
+                direction = 1f;
+                rb.velocity = new Vector2(0, up);
+            }
+            else if (Input.GetKeyUp("w"))
+            {
+                direction = 0f;
+                rb.velocity = new Vector2(0, 0);
+            }
+
+            if (Input.GetKey("s"))
+            {
+                direction = -1f;
+                rb.velocity = new Vector2(0, down);
+            }
+            else if (Input.GetKeyUp("s"))
+            {
+                direction = -0f;
+                rb.velocity = new Vector2(0, 0);
+            }
         }
-        else if (Input.GetKeyUp("w"))
+        else
         {
-            rb.velocity = new Vector2(0, 0);
+            if (Input.GetKey("up"))
+            {
+                direction = 1f;
+                rb.velocity = new Vector2(0, up);
+            }
+            else if (Input.GetKeyUp("up"))
+            {
+                direction = 0f;
+                rb.velocity = new Vector2(0, 0);
+            }
+
+            if (Input.GetKey("down"))
+            {
+                direction = -1f;
+                rb.velocity = new Vector2(0, down);
+            }
+            else if (Input.GetKeyUp("down"))
+            {
+                direction = -0f;
+                rb.velocity = new Vector2(0, 0);
+            }
         }
 
-        if (Input.GetKey("s"))
-        {
-            rb.velocity = new Vector2(0, down);
-        }
-        else if (Input.GetKeyUp("s"))
-        {
-            rb.velocity = new Vector2(0, 0);
-        }
     }
     void OnCollisionEnter2D(Collision2D col)
     {
